6efe251073ce363cbd0b210844f750be56210bf0
Pete Volikov <petevetep@users.noreply.github.com>
2015-04-25 01:32:30 +0300

    first commit. Task seems to be completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow to save CPU time on the second 
+## and the following requests to calculate inverted matrix
+## for the same original matrix
+##
+## No magic here. Just using two environment variables:
+## cached_matrix     for the original matrix
+## and 
+## inverted_matrix   for saving inverted matrix value
 
-## Write a short comment describing this function
+
+## makeCacheMatrix function just saves the original matrix
+## and initializes the cache 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverted_matrix <<- NULL
+  original_matrix <<- x
+  return (x)
 }
 
 
-## Write a short comment describing this function
+## cachSolve function either computes an inverted matrix
+## or just returns it from the cache
+##
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Just solve a matrix that is not assumed to be cached
+  if (!identical(x, original_matrix) ) {
+    return(solve(x))
+  } else {   
+    ## Check whether cache is still empty
+    if ( is.null(inverted_matrix) ) {
+      inverted_matrix <<- solve(x)
+    }
+     ## Return the value from cache
+    return(inverted_matrix) 
+  }
 }
